7860dc868111918362617d0988fc079a58027dab
ndkjing <ndkjing1@gmail.com>
2021-08-26 08:32:47 +0800

    update read data

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.lines import Line2D
 import matplotlib
+import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 
 import config
@@ -39,8 +40,10 @@ class ComThread(QThread):
 class Figure_Canvas(FigureCanvas):
     def __init__(self, parent=None, width=3.9, height=2.7, dpi=100):
         self.fig = Figure(figsize=(width, height), dpi=100)
+        plt.grid()
         super(Figure_Canvas, self).__init__(self.fig)
         self.ax = self.fig.add_subplot(111)
+        self.ax.grid()
 
     def test(self):
         x = [1, 2, 3, 4, 5, 6, 7]
